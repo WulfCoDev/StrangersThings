0a8787f0cdce0945c9d735c3e465f5e12a52454a
WulfCoDev <126393550+WulfCoDev@users.noreply.github.com>
2023-09-01 03:40:51 +0000

    edit token management login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { logIn } from './auth';
 
-const APIURL = `https://strangers-things.herokuapp.com/api/2302-ACC-ET-WEB-PT-D`;
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [token, setToken] = useTokenState();
+
   const handleLogin = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
 
     try {
-      const response = await fetch(APIURL + '/api/login', {
+      const response = await fetch(APIURL + '/users/login', {
         method: 'POST',
-        body: formData,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            username: formData.get('username'),
+            password: formData.get('password'),
+          },
+        }),
       });
 
       if (response.ok) {
         const data = await response.json();
         logIn(data.token);
+        setToken(data.token); // Update token state
+        navigate('/userdashboard');
       } else {
         // Handle login error
       }
--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,8 +1,23 @@
-let token = '';
+import { useState } from 'react';
 
+let token = '';
 
 export const logIn = newToken => {
   token = newToken;
+  sessionStorage.setItem('token', newToken);
+  // Update state with the new token
+  setTokenState(newToken);
+};
+
+export const getToken = () => {
+  return token;
+};
+
+const useTokenState = () => {
+  const [tokenState, setTokenState] = useState(
+    sessionStorage.getItem('token') || ''
+  );
+  return [tokenState, setTokenState];
 };
 
 
